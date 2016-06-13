08e270f064f68ce2669fc471d2a3f14888f83088
Roman Eyafi <romaneyafi@gmail.com>
2016-06-13 08:13:11 +0000

    Distance between two point

---
+++ distance.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+float d,v,t;
+printf("Enter the velocity in meter/second: ");
+scanf("%f",&v);
+printf("Enter the time in second:");
+scanf("%f",&t);
+d=v*t;
+printf("Distance=%0.2f meter",d);
+printf("\nCREATED BY EYAFI :p ");
+getch();
+return 0;
+
+
+}
